d2f794e3cbcee066948f06ea5d9c2101b5dd515b
emielhouman <emielhouman@gmail.com>
2025-06-01 17:41:01 +0200

    fix: highlight active menu items in navigation component for subroutes

--- src/components/@navigation/Navigation/components/NavigationMenu/components/NavigationMenuMain/NavigationMenuMain.tsx
+++ src/components/@navigation/Navigation/components/NavigationMenu/components/NavigationMenuMain/NavigationMenuMain.tsx
@@ -1,47 +1,38 @@
-import { useLocation } from "react-router-dom";
+import { useLocation } from 'react-router-dom';
 
-import {
-  ArtworksIcon,
-  CollectionsIcon,
-  HomeIcon,
-  InsightsIcon,
-} from "components/@icon";
+import { ArtworksIcon, CollectionsIcon, HomeIcon, InsightsIcon } from 'components/@icon';
 
-import NavigationMenuItem from "../NavigationMenuItem";
-import NavigationMenuList from "../NavigationMenuList";
+import NavigationMenuItem from '../NavigationMenuItem';
+import NavigationMenuList from '../NavigationMenuList';
 
 const NavigationMenuMain = () => {
   const location = useLocation();
 
   const isActiveLink = (path: string) => {
-    return location.pathname === path;
+    if (path === '/') return location.pathname === path;
+    return location.pathname.startsWith(path);
   };
 
   return (
     <NavigationMenuList>
-      <NavigationMenuItem
-        icon={HomeIcon}
-        label="Home"
-        to="/"
-        isActive={isActiveLink("/")}
-      />
+      <NavigationMenuItem icon={HomeIcon} label="Home" to="/" isActive={isActiveLink('/')} />
       <NavigationMenuItem
         icon={CollectionsIcon}
         label="Collections"
         to="/collections"
-        isActive={isActiveLink("/collections")}
+        isActive={isActiveLink('/collections')}
       />
       <NavigationMenuItem
         icon={ArtworksIcon}
         label="3D Objects"
         to="/objects"
-        isActive={isActiveLink("/objects")}
+        isActive={isActiveLink('/objects')}
       />
       <NavigationMenuItem
         icon={InsightsIcon}
         label="Insights"
         to="/insights"
-        isActive={isActiveLink("/insights")}
+        isActive={isActiveLink('/insights')}
       />
     </NavigationMenuList>
   );
