cebad2b500f8153b345e4156bcc8d0b1b774d1f3
emielhouman <emielhouman@gmail.com>
2025-06-01 18:02:23 +0200

    feat: created collections page with artist collections data from API (#8)

--- src/pages/Collections.tsx
+++ src/pages/Collections.tsx
@@ -1,17 +1,28 @@
-import { useCollections } from "queries/collections/useCollections";
-import { useState } from "react";
+import { useCollections } from 'queries/collections/useCollections';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
-import MenuButton from "components/@button/MenuButton";
-import Navigation from "components/@navigation/Navigation";
-import EmptyState from "components/@state/EmptyStates/EmptyState";
+import IconButton from 'components/@button/IconButton';
+import MenuButton from 'components/@button/MenuButton';
+import CollectionCard from 'components/@collections/CollectionCard';
+import CreateCollectionButton from 'components/@collections/CreateCollectionButton';
+import { HorizontalDivider } from 'components/@common/Divider';
+import { PlusIcon } from 'components/@icon';
+import Navigation from 'components/@navigation/Navigation';
+import EmptyState from 'components/@state/EmptyStates/EmptyState';
 
 const Collections = () => {
-  const { data } = useCollections();
+  const navigate = useNavigate();
+
+  const { data: collections, isLoading, isError } = useCollections();
+
+  // TODO: Add loading & error states
+  console.log(collections, isLoading, isError);
 
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleMenu = () => {
-    setIsMenuOpen((prev) => !prev);
+    setIsMenuOpen(prev => !prev);
   };
 
   return (
@@ -19,18 +30,38 @@ const Collections = () => {
       <Navigation isMenuOpen={isMenuOpen} />
       <div className="min-h-dvh px-8 pt-6 flex flex-col">
         <div className="flex justify-between items-center">
-          <h1 className="font-title font-semibold text-2xl text-neutral-50 pt-0.5">
+          <h1 className="font-title font-semibold text-[1.65rem] text-neutral-50 pt-0.5">
             Collections
           </h1>
           <MenuButton onClick={toggleMenu} isMenuOpen={isMenuOpen} />
         </div>
-        {data && data.collections.length === 0 && (
+        {collections && collections.length === 0 && (
           <EmptyState
             title="No collections..."
             description="Ready to go live? Let's build your first 3D collection and launch it into the world!"
             cta="Create collection"
           />
         )}
+        {collections && collections.length > 0 && (
+          <div className="flex flex-col gap-6 mt-8 mb-8">
+            <CreateCollectionButton />
+            <HorizontalDivider />
+            <div className="grid grid-cols-1 gap-5">
+              {collections.map(collection => (
+                <Link key={collection._id} to={`/collections/${collection._id}`}>
+                  <CollectionCard collection={collection} />
+                </Link>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="fixed inset-x-0 bottom-0 flex justify-center items-end mb-6">
+        <IconButton
+          icon={PlusIcon}
+          onClick={() => navigate('/collections/create')}
+          className="h-14 w-14 text-primary-500 border-2 border-neutral-100/10 shadow-lg rounded-lg p-2"
+        />
       </div>
     </>
   );
