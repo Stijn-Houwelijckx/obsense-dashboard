66eb27a52bd58d376a0d8484286e770a56f4f869
emielhouman <emielhouman@gmail.com>
2025-06-01 17:45:57 +0200

    chore: updated InputField to match new IconButton component structure

--- src/components/@form/InputField/InputField.tsx
+++ src/components/@form/InputField/InputField.tsx
@@ -1,14 +1,9 @@
-import { useState } from "react";
-import {
-  FieldErrors,
-  Path,
-  RegisterOptions,
-  UseFormRegister,
-} from "react-hook-form";
-import { Link } from "react-router-dom";
+import { useState } from 'react';
+import { FieldErrors, Path, RegisterOptions, UseFormRegister } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
-import IconButton from "components/@button/IconButton";
-import { HideIcon, ViewIcon } from "components/@icon";
+import IconButton from 'components/@button/IconButton';
+import { HideIcon, ViewIcon } from 'components/@icon';
 
 interface Props<T extends Record<string, unknown>> {
   label: string;
@@ -42,29 +37,24 @@ const InputField = <T extends Record<string, unknown>>({
       <div className="relative">
         <input
           className="w-full border bg-secondary-700 placeholder-neutral-500 rounded-lg px-3 p-2.5 focus:outline-none focus:border-primary-500"
-          type={showPassword ? "text" : type}
+          type={showPassword ? 'text' : type}
           placeholder={placeholder}
           {...register(name, validation)}
         />
         {isPassword && (
           <IconButton
             icon={showPassword ? HideIcon : ViewIcon}
-            btnClassName="absolute inset-y-0 right-0 text-neutral-500 bg-transparent px-3 p-2.5"
             onClick={() => setShowPassword((prev) => !prev)}
+            className="absolute inset-y-0 right-0 text-neutral-500 bg-transparent px-3 p-2.5"
           />
         )}
       </div>
       <div className="min-h-5 w-full flex justify-between">
         {errors[name] && (
-          <p className="text-sm text-red-500">
-            {errors[name]?.message?.toString()}
-          </p>
+          <p className="text-sm text-red-500">{errors[name]?.message?.toString()}</p>
         )}
         {hasForgotPassword && (
-          <Link
-            to="/forgot-password"
-            className="font-bold text-sm text-primary-500 ml-auto"
-          >
+          <Link to="/forgot-password" className="font-bold text-sm text-primary-500 ml-auto">
             Forgot password?
           </Link>
         )}
