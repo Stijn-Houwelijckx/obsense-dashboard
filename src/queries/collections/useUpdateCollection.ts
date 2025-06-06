ceef218fe815863de169bc69181efdab619d76f7
emielhouman <emielhouman@gmail.com>
2025-06-06 15:59:33 +0200

    feat: created useUpdateCollection React Query hook with optimistic updates (#21)

---
+++ src/queries/collections/useUpdateCollection.ts
@@ -0,0 +1,45 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { QueryKeys } from 'queries/QueryKeys';
+import { collectionsService } from 'services/collections';
+
+const updateCollection = async ({ id, collection }: { id: string; collection: FormData }) => {
+  const { data } = await collectionsService.updateCollection(id, collection);
+  return data;
+};
+
+export const useUpdateCollection = () => {
+  const queryClient = useQueryClient();
+
+  const { mutate, mutateAsync, isPending } = useMutation({
+    mutationFn: updateCollection,
+    onMutate: async ({ id, collection }) => {
+      await queryClient.cancelQueries({
+        queryKey: QueryKeys.collections.details(id),
+      });
+
+      const previousCollection = queryClient.getQueryData(QueryKeys.collections.details(id));
+      queryClient.setQueryData(QueryKeys.collections.details(id), collection);
+
+      return { previousCollection, id };
+    },
+    onError: (_, __, context) => {
+      if (context?.previousCollection && context?.id) {
+        queryClient.setQueryData(QueryKeys.collections.details(context.id), context.previousCollection);
+      }
+    },
+    onSuccess: async (_, __, context) => {
+      if (context?.id) {
+        await queryClient.invalidateQueries({ queryKey: QueryKeys.collections.all() });
+        await queryClient.invalidateQueries({
+          queryKey: QueryKeys.collections.details(context.id),
+        });
+      }
+    },
+  });
+
+  return {
+    updateCollection: mutate,
+    updateCollectionAsync: mutateAsync,
+    isLoading: isPending,
+  };
+};
