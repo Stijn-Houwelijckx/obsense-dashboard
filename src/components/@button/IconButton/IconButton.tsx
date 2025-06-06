f3988562bc46e1bd4eb30eb8e846aa3aa2be93a8
emielhouman <emielhouman@gmail.com>
2025-06-06 13:49:59 +0200

    chore: updated the IconButton component

--- src/components/@button/IconButton/IconButton.tsx
+++ src/components/@button/IconButton/IconButton.tsx
@@ -2,14 +2,16 @@ import { cn } from 'utils/cn';
 
 interface Props {
   className?: string;
+  disabled?: boolean;
   icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
   onClick?: () => void;
 }
 
-const IconButton = ({ className, icon: Icon, onClick }: Props) => {
+const IconButton = ({ className, disabled, icon: Icon, onClick }: Props) => {
   return (
     <button
       type="button"
+      disabled={disabled}
       onClick={onClick}
       className={cn(
         'h-12 w-12 flex items-center justify-center bg-secondary-800 rounded-full p-3',
