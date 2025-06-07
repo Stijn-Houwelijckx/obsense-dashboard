41422b1ba6d7b9143a2a587beb6f446736a28dde
emielhouman <emielhouman@gmail.com>
2025-06-07 17:27:49 +0200

    feat: added objects service endpoints for fetching 3D objects (#22)

---
+++ src/services/objects.ts
@@ -0,0 +1,35 @@
+import { ApiResponse } from 'types/api.types';
+import { Objects } from 'types/object.types';
+
+import api from './api';
+
+export const objectsService = {
+  getObjects: async () => {
+    const response = await api.get<ApiResponse<Objects>>('/objects');
+
+    if (response.status === 204) {
+      return {
+        code: 204,
+        status: 'success',
+        message: 'No objects found.',
+        data: { objects: [] },
+      };
+    }
+
+    return response.data;
+  },
+  getCollectionObjects: async (collectionId: string) => {
+    const response = await api.get<ApiResponse<Objects>>(`/objects/collections/${collectionId}`);
+
+    if (response.status === 204) {
+      return {
+        code: 204,
+        status: 'success',
+        message: 'No objects found.',
+        data: { objects: [] },
+      };
+    }
+
+    return response.data;
+  },
+};
