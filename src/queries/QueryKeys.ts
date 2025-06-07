53ed73db5804c5cd1fd6f1924f5881f9eb68a29c
emielhouman <emielhouman@gmail.com>
2025-06-07 17:41:59 +0200

    feat: added query keys for 3D objects with type-safe parameters (#22)

--- src/queries/QueryKeys.ts
+++ src/queries/QueryKeys.ts
@@ -1,9 +1,16 @@
+import { CollectionItem } from 'types/collection.types';
+import { ObjectItem } from 'types/object.types';
+
 export const QueryKeys = {
   collections: {
     all: () => ['collections'],
-    details: (id: string) => ['collections', id],
+    details: (id: CollectionItem['_id']) => ['collections', id],
   },
   genres: {
     all: () => ['genres'],
+  },
+  objects: {
+    all: () => ['objects'],
+    collection: (collectionId: ObjectItem['_id']) => ['objects', collectionId],
   },
 };
